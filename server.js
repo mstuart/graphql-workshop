c84f1cad707bb82bdaa066fe15159603791e3fa8
marstuart <marstuart@paypal.com>
2018-10-09 17:29:45 -0700

    step3

--- server.js
+++ server.js
@@ -22,6 +22,30 @@ const typeDefs = `
   }
 `;
 
+// EXERCISE #3 -- Ok, let's turn this up to eleven!
+//
+// Our Album type has a "userId", but that's not that useful to clients.
+//
+// If this was REST, clients would be expected to make another round-trip to
+// the API to fetch associated User data.  We can do better than that!
+//
+// This exercise is broken into 3 parts.
+//
+// Part #1 --
+// We want to allow clients to query for User data within an Album.
+// Let's create a "link" between Album and User.
+// Let's start by deprecating the "userId" field.  Add a @deprecated directive next to it.
+//
+// Part #2 --
+// Inspect the http://jsonplaceholder.typicode.com/users/1 API.
+// In this example, it's user #1.
+// Create some schema types for this User.
+// Hint: The "address" and "company" fields within User should be types.
+// Be mindful of the return types you're using.
+//
+// Part #3 --
+// Create a "user" field within the Album type that returns your new User type
+// To get user data, use the `invoke` async function and hit the `GET /users/<id>` endpoint
 const resolvers = {
   Query: {
     albums: async (rootObj, { albumId, userId }) => {
