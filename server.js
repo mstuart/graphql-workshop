5d4db98e94b0577cfd93755f64bb1663d39253cf
Mark Stuart <mstuart@users.noreply.github.com>
2018-10-09 17:14:21 -0700

    step23

--- server.js
+++ server.js
@@ -24,17 +24,19 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
+    // EXERCISE #2 -- Okay, so we have a way to fetch all of the Albums and
+    // a single Album.  What if our clients wanted to grab a subset of
+    // Albums that match any of the fields like "albumId" or "userId"
+    // Ex: If they're building a pagination or search UI
+    //
+    // Let's add two *optional* parameters that filters based on "userId" or "albumId"
+    // NOTE: Optional, not required.
+    // We still want people to be able to fetch all of the albums if they want to.
     albums: async () => await invoke('/albums'),
 
     album: async (rootObj, { albumId }) => {
       const albums = await invoke('/albums');
 
-      // We're using Array#find to search through the albums Array to find
-      // the album that has this albumId.  Otherwise, we're returning null.
-      //
-      // NOTE: The ID scalar in GraphQL can either be a String or Number.
-      // In our schema, "albumID" is an ID, so we need to cast albumId to
-      // a Number before comparing it.
       return albums.find(album => album.id === Number(albumId));
     }
   },
