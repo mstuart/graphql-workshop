1e4c4db2ccb047f0f8d76f782aa5d93847e28446
marstuart <marstuart@paypal.com>
2018-10-09 17:03:58 -0700

    step1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const typeDefs = `
   }
 
   type Album {
-    # The Album's ID (which is unique)
+    # The Album's ID
     albumId: ID
 
     # The User ID that is associated this album
@@ -28,12 +28,13 @@ const resolvers = {
     // By returning it, it's available to Album resolvers as "rootObj".
     // "rootObj" is used for passing data from parent-to-child.
     //
-    // EXERCISE #1 -- Currently, we're returning the entire list of albums
-    // It's likely that our clients may want a small subset of those albums
-    // if they're implementing pagination or a search feature, etc.
-    // Let's add an optional parameter to allow clients to filter by "albumId"
-    //
     albums: async () => await invoke('/albums')
+
+    // EXERCISE #1 -- Currently, we're returning the entire list of albums
+    //
+    // It's likely that our clients may want a single album with a given albumId.
+    // Let's create another field called "album" that takes a required parameter
+    // called "albumId" that returns that particular Album.
   },
 
   Album: {
