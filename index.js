d4ad90ce1e2b94c4eb7f7c3067ab503b2fed12b3
Mark Stuart <mstuart@users.noreply.github.com>
2018-10-10 18:31:45 -0700

    hi

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// exit early from incompatible node versions
-if (process.versions.node.match(/\d/)[0] < 8) {
-  console.error('Please run node v8 or newer.');
-  return;
+//
+//
+//
+//
+//
+//
+// If you're reading this, check out server.js instead.
+// Nothing to see here!
+//
+//
+//
+//
+//
+if (process.versions.node.split('.')[0] < 8) {
+  console.error('Please run node version 8 or better.');
+  process.exit(1);
 }
 
 require('@babel/register');
