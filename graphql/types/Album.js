ce8cceefd71018c5a6ff69c78f66826342d4367a
Mark Stuart <mstuart@users.noreply.github.com>
2018-10-14 16:08:34 -0700

    step7

--- graphql/types/Album.js
+++ graphql/types/Album.js
@@ -3,6 +3,12 @@ export default {
     albumId: ({ id }) => id,
 
     user: async ({ userId }, args, { dataLoader }) =>
-      await dataLoader.load(`/users/${userId}`)
+      await dataLoader.load(`/users/${userId}`),
+
+    photos: async ({ id }, args, { dataLoader }) => {
+      const photos = await dataLoader.load(`/photos?albumId=${id}`);
+
+      return photos.slice(0, 10);
+    }
   }
 };
