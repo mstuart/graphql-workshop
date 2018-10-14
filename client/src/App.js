693665e041578ea94960b0ad03a41257669fd0d1
marstuart <marstuart@paypal.com>
2018-10-14 16:11:16 -0700

    step7-solution

--- client/src/App.js
+++ client/src/App.js
@@ -2,77 +2,48 @@ import React from 'react';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
 
-// If everything is working, you should see "hi!" in hot pink at localhost:3000
-const App = () => {
-  return <h1 style={{ color: 'hotpink', fontSize: '5em' }}>hi!</h1>;
-};
+const GET_ALBUMS = gql`
+  query {
+    albums {
+      title
+      user {
+        name
+      }
+      photos {
+        title
+        thumbnailUrl
+      }
+    }
+  }
+`;
 
-// @TODO Uncomment me and delete the other App component above.
-//
-// // NOTE: You'll have an error in your browser, but don't worry.  We'll fix it later.
-//
-// //
-// // It's worth calling out that this query is co-located next to its markup below.
-// //
-// // This is an important dev feature that allows for queries to stay lean over time.
-// // Because a query is next to the component that uses it, it's more likely that a
-// // developer will add/remove fields from a query as they modify their component
-// // over time.  So you'll never overfetch data.  It also makes for code that's
-// // easier to debug and understand.
-// const GET_ALBUMS = gql`
-//   query {
-//     albums {
-//       title
-//       user {
-//         name
-//       }
-//     }
-//   }
-// `;
-//
-// const App = () => (
-//   <Query query={GET_ALBUMS}>
-//     {({ loading, error, data }) => {
-//       // Apollo provides some nice indicators for loading and error states
-//       // in case you wanted to show a spinner or handle errors within your component.
-//       // The rest of your API data is available under "data"
-//       if (loading) return <div>Loading...</div>;
-//       if (error) return <div>`Error :( ${error}`</div>;
-//
-//       //
-//       // EXERCISE #7 --
-//       //
-//       // Let's loop over our albums and display their title, the user that created them, and the album's photos.
-//       // Our Album type now has a photos field that returns an album's photos.
-//       // See graphql/types/Album.graphql and graphql/types/Photo.graphql
-//       //
-//       // Step 1) The title and user name are placeholders.  Replace PLACEHOLDER with {album.someproperty}
-//       //
-//       // Step 2) Modify the GET_ALBUMS query (at the top) to pull photos and their thumbnailUrl and title.
-//       //
-//       // Step 3) If all is well, the error should go away and you should see a list of albums, their titles,
-//       //         creators and their images!
-//       return data.albums.map(album => {
-//         return (
-//           <div>
-//             <h1>"PLACEHOLDER"</h1>
-//             <h3>By: PLACEHOLDER</h3>
-//
-//             {album.photos.map(photo => {
-//               return (
-//                 <img
-//                   src={photo.thumbnailUrl}
-//                   height="150"
-//                   width="150"
-//                   alt={photo.title}
-//                 />
-//               );
-//             })}
-//           </div>
-//         );
-//       });
-//     }}
-//   </Query>
-// );
+const App = () => (
+  <Query query={GET_ALBUMS}>
+    {({ loading, error, data }) => {
+      if (loading) return <div>Loading...</div>;
+      if (error) return <div>`Error :( ${error}`</div>;
+
+      return data.albums.map(album => {
+        return (
+          <div>
+            <h1>"{album.title}"</h1>
+            <h3>By: {album.user.name}</h3>
+
+            {album.photos.map(photo => {
+              return (
+                <img
+                  src={photo.thumbnailUrl}
+                  height="150"
+                  width="150"
+                  alt={photo.title}
+                />
+              );
+            })}
+          </div>
+        );
+      });
+    }}
+  </Query>
+);
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-// apollo-boost is a quick way to get started w/ apollo-client.
-// Read https://github.com/apollographql/apollo-client/tree/master/packages/apollo-boost
 import ApolloClient from 'apollo-boost';
-
-// ApolloProvider makes your apollo client available to your React component tree
 import { ApolloProvider } from 'react-apollo';
-
-// Importing the App we're going to be working on.  See client/src/App.js
 import App from './App';
 
 import './index.css';
 
-// Create an Apollo client, passing it the URL of our GraphQL API.
-// This is the simplest way to get started.
-// In a real world scenario, you'd likely want to tune the cache and batch settings..
-// Here are some other options: https://www.apollographql.com/docs/react/essentials/get-started.html#configuration
 const client = new ApolloClient({
   uri: 'http://localhost:4000'
 });
 
-// This is where we're rendering our React app.
-// Using ApolloProvider to provide our client to our App component.
-// And we're injecting our React app into the DOM.
 render(
   <ApolloProvider client={client}>
     <App />
